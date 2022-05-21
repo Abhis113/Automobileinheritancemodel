341cd55c9e7e2aeb32fb92d7526e0a11a44940a1
Abhishek kumar <101392875+Abhis113@users.noreply.github.com>
2022-05-21 20:34:47 +0530

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,10 @@
+function Nano(type) {
+    this.type=type;
+    this.brand="Tata";
+    (this.logo="URL"),(this.seller="Tata")
+   
+  }
+let p1=new Nano("Harrier");
+console.log(p1);
+let p2=new Nano("Nexon");
+console.log(p2);
