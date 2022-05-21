6ff7f071741a0197b43363e66fbcb55c090d4cf2
Abhishek kumar <101392875+Abhis113@users.noreply.github.com>
2022-05-21 19:54:04 +0530

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,9 @@
+const tata_Tiago={
+    name:"Tata Tiago",
+    Engine:"Petrol Engine",
+    seat:"5 seater",
+    mileage:"25 km/kg",
+    wheelbase:2400,
+};
+const tata_nexon=Object.create(tata_Tiago);
+console.log(tata_nexon);
---
+++ index5.js
@@ -0,0 +1,10 @@
+function Nano() {
+
+    name="Tata";
+    engine="Petrol Engine";
+    seat="5 seater";
+    mileage="25 km/kg";
+    wheelbase=2400;
+  }
+let p1=new Nano();
+console.log(p1);
